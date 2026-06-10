1675d577a3fcaca168431b558b723b169b3321ea
lipe_kleiz <lipe.kleiz@gmail.com>
2026-06-10 19:54:28 -0300

    feat: config inicial do projeto spring boot com jdk21 projeto criado via spring init dependencies: spring web, jpa, h2, devtools. configurado para java21 estrutura base do maven

---
+++ src/test/java/com/lipe_kleiz/delivery_api/DeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lipe_kleiz.delivery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
